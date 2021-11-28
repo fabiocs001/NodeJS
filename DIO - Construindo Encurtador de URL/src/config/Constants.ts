cf431ee18066311303bdc93cd708c641761e84d7
Fábio <fabioc.silva@gmail.com>
2021-11-27 23:47:46 -0300

    Revert "Construindo Encurtador de URL"
    This reverts commit fb0444ebb36fe1da67d190d9151653b7f30a1aaa.

--- DIO - Construindo Encurtador de URL/src/config/Constants.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-	API_URL: 'http://localhost:5000',
-	MONGO_CONNECTION:
-		'mongodb+srv://Admin:aE1xa0GNyb2V9C89@clusters.d2kij.mongodb.net/Clusters?retryWrites=true&w=majority',
-		//'mongodb+srv://Admin:aE1xa0GNyb2V9C89@clusters.d2kij.mongodb.net/Clusters?retryWrites=true&w=majority',
-}
