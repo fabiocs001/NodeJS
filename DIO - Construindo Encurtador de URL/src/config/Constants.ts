fb0444ebb36fe1da67d190d9151653b7f30a1aaa
Fábio <fabioc.silva@gmail.com>
2021-11-27 21:06:07 -0300

    Construindo Encurtador de URL

---
+++ DIO - Construindo Encurtador de URL/src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://Admin:aE1xa0GNyb2V9C89@clusters.d2kij.mongodb.net/Clusters?retryWrites=true&w=majority',
+		//'mongodb+srv://Admin:aE1xa0GNyb2V9C89@clusters.d2kij.mongodb.net/Clusters?retryWrites=true&w=majority',
+}
